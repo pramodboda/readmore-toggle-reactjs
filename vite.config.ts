a7956b4940eac5024abfaebf74aa7b376084a954
Pramod Boda <PB493939@Haleon.ad>
2024-10-14 21:59:11 +0530

    Initial project files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/readmore-toggle-reactjs/", // YOUR REPO NAME
   plugins: [react()],
-})
+});
