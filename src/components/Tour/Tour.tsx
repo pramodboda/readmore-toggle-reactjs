30405eed9f7d6ac75bdf1dc256db4835e2a878f6
Pramod Boda <PB493939@Haleon.ad>
2024-10-15 21:29:24 +0530

    UPDATE: Read More functionality

--- src/components/Tour/Tour.tsx
+++ src/components/Tour/Tour.tsx
@@ -10,19 +10,70 @@ import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 
-const preventDefault = (event: React.SyntheticEvent) => event.preventDefault();
+// const preventDefault = (event: React.SyntheticEvent) => event.preventDefault();
 
-const Tour = ({ id, image, info, name, price }) => {
+interface TourProps {
+  id: string;
+  image: string;
+  name: string;
+  info: string;
+  price: string;
+}
+
+// const Tour: React.FC<TourProps> = ({ id, image, info, name, price }) => {
+//   const [readMore, setReadMore] = useState(false);
+//   // Single tour component
+//   return (
+//     <Card sx={{ maxWidth: 345 }} id={id}>
+//       <CardMedia
+//         component="img"
+//         alt="green iguana"
+//         height="140"
+//         image={image}
+//       />
+//       <CardContent>
+//         <Typography gutterBottom variant="h6" component="div">
+//           {name}
+//         </Typography>
+//         <Typography
+//           gutterBottom
+//           variant="h5"
+//           component="div"
+//           sx={{ color: "#ff7300" }}
+//         >
+//           {price}/-
+//         </Typography>
+//         <Typography variant="body2" sx={{ color: "text.secondary" }}>
+//           {readMore ? info : `${info.substring(0, 200)}...`}
+//           <Box>
+//             <Link
+//               href="#"
+//               underline="none"
+//               onClick={(e) => {
+//                 e.preventDefault();
+//                 setReadMore(!readMore);
+//               }}
+//             >
+//               {readMore ? "show less" : "read more"}
+//             </Link>
+//           </Box>
+//         </Typography>
+//       </CardContent>
+//       <CardActions>
+//         <Button size="small">Not interested</Button>
+//       </CardActions>
+//     </Card>
+//   );
+// };
+
+// export default Tour;
+
+const Tour: React.FC<TourProps> = ({ id, image, info, name, price }) => {
   const [readMore, setReadMore] = useState(false);
-  // Single tour component
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={image}
-      />
+    <Card sx={{ maxWidth: 345 }} id={id}>
+      <CardMedia component="img" alt="tour image" height="140" image={image} />
       <CardContent>
         <Typography gutterBottom variant="h6" component="div">
           {name}
@@ -37,11 +88,12 @@ const Tour = ({ id, image, info, name, price }) => {
         </Typography>
         <Typography variant="body2" sx={{ color: "text.secondary" }}>
           {readMore ? info : `${info.substring(0, 200)}...`}
-          <Box onClick={preventDefault}>
+          <Box>
             <Link
               href="#"
               underline="none"
-              onClick={() => {
+              onClick={(e) => {
+                e.preventDefault();
                 setReadMore(!readMore);
               }}
             >
